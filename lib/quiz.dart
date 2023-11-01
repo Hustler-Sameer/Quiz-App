9d06815a87e5f63fff6a896b1bd1377908608bd2
Hustler-Sameer <sameerkurkure10@gmail.com>
2023-11-01 16:36:40 +0530

    1st nov

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -15,30 +15,62 @@ class Quiz extends StatefulWidget {
   }
 }
 
+// * Using Init to load screen method
+
+// class _QuizState extends State<Quiz> {
+//   // this method cannot be used as the method is created when the active screen is also
+//   //also initialized hence we need to use init method.
+//   // Widget activeScreen = StartScreen(switchScreen);
+
+//   // void switchScreen() {
+//   //   setState(() {
+//   //     activeScreen = const QuestionsScreen();
+//   //   });
+//   // }
+
+//   // this let us execute before the build method is executed hence no error is caused
+//   Widget? activeScreen;
+//   // the widget has question mark to indicate that the value can also be null
+//   @override
+//   void initState() {
+//     print('init executed');
+//     activeScreen = StartScreen(switchScreen);
+//     super.initState();
+//   }
+
+//   void switchScreen() {
+//     setState(() {
+//       activeScreen = const QuestionsScreen();
+//     });
+//   }
+
+//   @override
+//   Widget build(context) {
+//     return MaterialApp(
+//         title: 'Flutter Demo',
+//         home: Scaffold(
+//             body: Container(
+//                 decoration: BoxDecoration(
+//                     gradient: LinearGradient(
+//                   begin: startAlignment,
+//                   end: endAlignment,
+//                   colors: const [
+//                     Color.fromARGB(90, 0, 0, 0),
+//                     Color.fromARGB(200, 53, 4, 4)
+//                   ],
+//                 )),
+//                 child: activeScreen)));
+//   }
+// }
+
+//* using terniary operations to load screen
+
 class _QuizState extends State<Quiz> {
-  // this method cannot be used as the method is created when the active screen is also
-  //also initialized hence we need to use init method.
-  // Widget activeScreen = StartScreen(switchScreen);
-
-  // void switchScreen() {
-  //   setState(() {
-  //     activeScreen = const QuestionsScreen();
-  //   });
-  // }
-
-  // this let us execute before the build method is executed hence no error is caused
-  Widget? activeScreen;
-  // the widget has question mark to indicate that the value can also be null
-  @override
-  void initState() {
-    print('init executed');
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -47,16 +79,20 @@ class _QuizState extends State<Quiz> {
     return MaterialApp(
         title: 'Flutter Demo',
         home: Scaffold(
-            body: Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                  begin: startAlignment,
-                  end: endAlignment,
-                  colors: const [
-                    Color.fromARGB(90, 0, 0, 0),
-                    Color.fromARGB(200, 53, 4, 4)
-                  ],
-                )),
-                child: activeScreen)));
+          body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: startAlignment,
+              end: endAlignment,
+              colors: const [
+                Color.fromARGB(90, 0, 0, 0),
+                Color.fromARGB(200, 53, 4, 4)
+              ],
+            )),
+            child: activeScreen == 'start-screen'
+                ? StartScreen(switchScreen)
+                : const QuestionsScreen(),
+          ),
+        ));
   }
 }
